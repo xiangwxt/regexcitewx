6ce1f5b261e4cb4e5d44a89cb64827c5956fb16c
Wenxin Xiang <xiangwx@student.ubc.ca>
2022-01-17 18:47:37 -0800

    edit readme and test

--- tests/testthat/test-str_split_one.R
+++ tests/testthat/test-str_split_one.R
@@ -1,6 +1,3 @@
-test_that("strsplit1() split a string", {
-  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
-})
 test_that("str_split_one() splits a string", {
   expect_equal(str_split_one("a,b,c", ","), c("a", "b", "c"))
 })
